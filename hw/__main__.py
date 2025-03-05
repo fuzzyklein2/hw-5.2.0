36f8a1b09c0fe1e797a4fff1c50b2f1863176ef6
Fuzzy Klein <157883031+fuzzyklein2@users.noreply.github.com>
2025-03-05 06:26:24 -0600

    Initial commit

---
+++ hw/__main__.py
@@ -0,0 +1,13 @@
+if __debug__:
+    breakpoint()
+
+from .startup import *
+from .filter import Filter
+
+if __name__ == '__main__':
+    # print(PROGRAM)
+    log.info(f'Executing {PROGRAM} ...')
+    
+    p = Filter()
+    
+    # log.info(f'Execution complete.')
